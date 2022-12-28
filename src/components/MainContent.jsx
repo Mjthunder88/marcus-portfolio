958e29028872aa67b732148e392301c6bb1666ba
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-12-27 17:03:29 -0700

    fixed typo

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,7 +8,7 @@ const MainContent = () => {
         <section className={styles.section}>
             <div className={styles.prime_content}>
                 <article className={styles.inner_prime_content}>
-                <h1 className={styles.prime_header}>Full Stack Developer</h1>
+                <h1 className={styles.prime_header}>Full-Stack Developer</h1>
                 <p> I Enjoy creating full-stack applications with a responsive UI to create a better user experince.</p>
                 </article>
             <img src={fresh} alt="profile_pic" className={styles.fresh} />
