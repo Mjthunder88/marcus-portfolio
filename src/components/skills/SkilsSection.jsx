ff6d6daefd5218c350114de7513466abc2a373cc
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-01-02 12:30:32 -0700

    added more css to the skills section

--- src/components/skills/SkilsSection.jsx
+++ src/components/skills/SkilsSection.jsx
@@ -7,10 +7,12 @@ const SkillsSection = () => {
         <section className={styles.section}>
             <div className={styles.primary_content}>
             <article className={styles.inner_top}>
-                <h1>Projects</h1>
+                <h1>Personal Projects</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, ratione? Nisi soluta modi maiores molestiae asperiores facilis ipsam nostrum quo similique odit a magnam itaque cum laudantium, architecto rerum odio?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi velit sed voluptatibus nostrum suscipit magnam sint. Molestias exercitationem sequi ad, repellendus adipisci, minus nobis expedita ut magni eligendi qui quo.lore</p>
             </article>
             <article className={styles.inner_bottom}>
                 <h1>Tech Stack</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, ratione? Nisi soluta modi maiores molestiae asperiores facilis ipsam nostrum quo similique odit a magnam itaque cum laudantium, architecto rerum odio?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi velit sed voluptatibus nostrum suscipit magnam sint. Molestias exercitationem sequi ad, repellendus adipisci, minus nobis expedita ut magni eligendi qui quo.lore</p>
             </article>
             </div>
         </section>
